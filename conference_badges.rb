07e5dbf04482cc10139c715d275f016e813e5026
Dakota Wicker <dwicker1996@gmail.com>
2020-03-25 22:33:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,8 @@
-def badge_maker(name)
+def badge_maker(name = "Arel")
  return "Hello, my name is #{name}."
 end
 
-speakers = [ "Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-
 def batch_badge_creator(speakers)
-  speakers.each {|speaker| puts "Hello my name is, "}
+  speakers.each do |badge| puts "Hello my name is, #{badge}"
  end
 end
