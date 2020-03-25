e81c93e0475bee747b32a435aa6bf4bf7a406399
Dakota Wicker <dwicker1996@gmail.com>
2020-03-25 22:22:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,4 +5,4 @@ end
 speakers = [ "Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(speakers)
-  
+  speakers.each {|speaker| puts "Hello my name is, {speakers}"}
