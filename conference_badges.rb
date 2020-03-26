8b4ee08cf3cec1cc9a68796eb5b017fae410b3f6
Dakota Wicker <dwicker1996@gmail.com>
2020-03-26 00:16:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,8 @@ def batch_badge_creator(attendees)
 end
 def assign_rooms(attendees)
   room_assign = []
-  attendees.each_with_index do |room_assign, index|
-    room_assign.push("Hello, #{attendees}! You'll be assigned to #{index + 1}")
-end
+  attendees.each_with_index do |name, index| # change room_assign to name
+    room_assign.push("Hello, #{name}! You'll be assigned to #{index + 1}") # change attendees to name
+  end
+  room_assign # return this now
 end
