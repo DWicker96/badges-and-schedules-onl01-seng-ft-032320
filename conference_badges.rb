231ab780d229f12c4e3689a25f2cfcd5274d3d64
Dakota Wicker <dwicker1996@gmail.com>
2020-03-26 00:17:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ end
 def assign_rooms(attendees)
   room_assign = []
   attendees.each_with_index do |name, index| # change room_assign to name
-    room_assign.push("Hello, #{name}! You'll be assigned to #{index + 1}") # change attendees to name
+    room_assign.push("Hello, #{name}! You'll be assigned to #{index + 1}!") # change attendees to name
   end
   room_assign # return this now
 end
