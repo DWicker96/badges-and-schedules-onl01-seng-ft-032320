d120a317dbf7668fd5332139df8fe478192635e0
Dakota Wicker <dwicker1996@gmail.com>
2020-03-26 00:25:00 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def badge_maker(name = "Arel")
+ return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map do |badge|
+  "Hello, my name is #{badge}."
+ end
+end
+def assign_rooms(attendees)
+  room_assign = []
+  attendees.each_with_index do |name, index| # change room_assign to name
+    room_assign.push("Hello, #{name}! You'll be assigned to room #{index + 1}!") # change attendees to name
+  end
+  room_assign # return this now
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |greeting|
+    puts greeting
+  end
+  assign_rooms(attendees).each do |greeting|
+    puts greeting
+  end
+
+end
